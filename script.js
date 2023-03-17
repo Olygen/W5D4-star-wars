6ff1792c799e42ceec2aba94b5d83bd9e4319b39
Oligen <olga.sarukhanova@gmail.com>
2023-03-17 13:19:02 -0400

    window.alerts with console.log

--- script.js
+++ script.js
@@ -50,18 +50,18 @@ class Spaceship {
         window.alert(`The alien ship's hull is ${alienShip.hull}`);
       } else {
         console.log("The attack missed");
-        this.window.alert("The attack missed");
+        window.alert("The attack missed");
       }
   
       if (alienShip.hull > 0 && USS_Assembly.hull > 0) {
         console.log("Alien ship attacks the USS Assembly");
-        this.window.alert("Alien ship attacks the USS Assembly");
+        window.alert("Alien ship attacks the USS Assembly");
         if (alienShip.attack(USS_Assembly)) {
           console.log(`The USS Assembly's hull is ${USS_Assembly.hull}`);
           this.window.alert(`The USS Assembly's hull is ${USS_Assembly.hull}`);
         } else {
           console.log("The attack missed");
-          this.window.alert("The attack missed");
+          window.alert("The attack missed");
         }
       }
     }
@@ -69,14 +69,14 @@ class Spaceship {
     // Output the result of the battle
     if (USS_Assembly.hull <= 0) {
       console.log("Game over. The USS Assembly was destroyed.");
-      this.window.alert("Game over. The USS Assembly was destroyed.");
+      window.alert("Game over. The USS Assembly was destroyed.");
     } else {
       console.log("Congratulations! You have destroyed the alien ship.");
-      this.window.alert("Congratulations! You have destroyed the alien ship.");
+      window.alert("Congratulations! You have destroyed the alien ship.");
     }
   } else {
     // If the user does not confirm, end the attack
     console.log("Attack aborted.");
-    this.window.alert("Attack aborted.");
+    window.alert("Attack aborted.");
   }
 });
